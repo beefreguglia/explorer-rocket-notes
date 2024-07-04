a3a228cd7ec3e1d59b8487905f5b194e3d206c7d
Bernardo <beefreguglia@gmail.com>
2024-07-04 19:46:18 -0300

    feature: create profile page and form

---
+++ src/pages/Profile/styles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+
+  > header {
+    width: 100%;
+    height: 144px;
+
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+      font-size: 24px;
+    }
+  }
+`;
+
+export const Form = styled.form`
+  max-width: 340px;
+  margin: -84px auto 0;
+`;
