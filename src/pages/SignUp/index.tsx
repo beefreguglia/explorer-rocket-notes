1793c4853adf72fca293801b04b990c609d8b242
Bernardo <beefreguglia@gmail.com>
2024-07-04 19:35:04 -0300

    feature: create signup page

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,34 @@
+import { FiLock, FiMail, FiUser } from "react-icons/fi";
+import { Input } from "../../components/Input";
+import { Background, Container, Form } from "./styles";
+import { Button } from "../../components/Button";
+
+export function SignUp() {
+  return(
+    <Container>
+      <Background />
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus link úteis</p>
+        <h2>Crie sua conta</h2>
+        <Input 
+          placeholder="Nome"
+          type="text"
+          icon={<FiUser size={20} />}
+        />
+        <Input 
+          placeholder="E-mail"
+          type="text"
+          icon={<FiMail size={20} />}
+        />
+        <Input 
+          placeholder="Senha"
+          type="password"
+          icon={<FiLock size={20} />}
+        />
+        <Button>Cadastrar</Button>
+        <a href="#">Voltar para o login</a>
+      </Form>
+    </Container>
+  );
+}
