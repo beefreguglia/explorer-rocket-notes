cab5efd186a6bf306d7fc354938d241490c95a55
Bernardo <beefreguglia@gmail.com>
2024-07-04 15:50:21 -0300

    feature: create create brand styles

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,8 +4,10 @@ export const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: grid;
+  padding: .5rem;
   grid-template-columns: 250px auto;
   grid-template-rows: 105px 128px auto 64px;
+  grid-column-gap: 0.5rem;
   grid-template-areas: 
   "brand header"
   "menu search"
@@ -17,6 +19,18 @@ export const Container = styled.div`
 
 export const Brand = styled.div`
   grid-area: brand;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  border-radius: 10px 10px 0 0;
+
+  h1 {
+    font-size: 24px;
+    color: orange;
+  }
 `;
 
 export const Menu = styled.ul`
